3c139e805b73d091fe1995ce9c969bb927ab0bf0
lambda-knight <tokomakoma123@gmail.com>
2026-08-01 18:32:41 +0900

    AnimatedEpisodeをダイナミックインポートに変更：SSR無効でクライアント側レンダリング

--- app/episodes/[date]/page.tsx
+++ app/episodes/[date]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import episodesData from "@/data/episodes.json";
 import type { Episode } from "@/types/episode";
 import { MathContent } from "./MathContent";
-import { AnimatedEpisode } from "./AnimatedEpisode";
+
+const AnimatedEpisode = dynamic(() => import("./AnimatedEpisode").then(mod => ({ default: mod.AnimatedEpisode })), { ssr: false });
 
 const episodes = episodesData as Episode[];
 
